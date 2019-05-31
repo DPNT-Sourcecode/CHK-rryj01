b2a32274e74b24bc7a1e5ff6d692bb7ac3c8d905
sbx_user1051 <sbx_user1051@169.254.178.101>
2019-05-31 12:12:27 +0000

    Fri May 31 12:12:27 UTC 2019

--- src/main/java/befaster/solutions/Items.java
+++ src/main/java/befaster/solutions/Items.java
@@ -1,4 +1,48 @@
 package befaster.solutions;
 
 public class Items {
+    private String itemA = "A";
+    private int priceA = 50;
+    private String itemB = "B";
+    private int priceB = 30;
+    private String itemC = "C";
+    private int priceC = 20;
+    private String itemD = "D";
+    private int priceD = 15;
+
+    public Items() {
+    }
+
+    public String getItemA() {
+        return itemA;
+    }
+
+    public int getPriceA() {
+        return priceA;
+    }
+
+    public String getItemB() {
+        return itemB;
+    }
+
+    public int getPriceB() {
+        return priceB;
+    }
+
+    public String getItemC() {
+        return itemC;
+    }
+
+    public int getPriceC() {
+        return priceC;
+    }
+
+    public String getItemD() {
+        return itemD;
+    }
+
+    public int getPriceD() {
+        return priceD;
+    }
 }
+
