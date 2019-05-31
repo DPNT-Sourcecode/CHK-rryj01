adecbca737f62312e535fd83d9f735d3ec085e6c
sbx_user1051 <sbx_user1051@169.254.178.101>
2019-05-31 11:49:13 +0000

    Fri May 31 11:49:13 UTC 2019

---
+++ src/main/java/befaster/solutions/HLO/HelloSolution2.java
@@ -0,0 +1,8 @@
+package befaster.solutions.HLO;
+
+public class HelloSolution2 {
+    public String hello(String friendName) {
+        //return "Hello " + friendName;
+        return "Hello, " + friendName + "!";
+    }
+}
